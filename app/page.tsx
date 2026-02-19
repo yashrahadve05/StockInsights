70c9558d9608f7ff0138dbb89a0e4d35daf16374
yashrahadve05 <yashrahadve05@gmail.com>
2026-02-19 16:32:42 +0530

    Project Initialized

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Stock Insights</Button>
+    </div>
+    );
+};
+
+export default Home;
