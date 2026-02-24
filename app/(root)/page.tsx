70afcc29d1e0cde996abe49cfc46b854fc5843b9
yashrahadve05 <yashrahadve05@gmail.com>
2026-02-24 21:36:20 +0530

    Add TradingView widgets and configuration for market overview and stock heatmap

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,47 @@
-import { Button } from "@/components/ui/button";
-
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants";
 
 const Home = () => {
-  return (
-    <div className="flex min-h-screen home-wrapper">
-      Home
-    </div>
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            <section className="grid w-full gap-8 home-section">
+                <div className="md:col-span-1 xl:col-span-1">
+                    <TradingViewWidget
+                        title="Market Overview"
+                        scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+                        // scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js"
+                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                        className="custom-chart"
+                        height={600}
+                    />
+                </div>
+                <div className="md:col-span-1 xl:col-span-2">
+                    <TradingViewWidget
+                        title="Stock Heatmap"
+                        scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js"
+                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                        height={600}
+                    />
+                </div>
+            </section>
+            <section className="grid w-full gap-8 home-section">
+                <div className="h-full md:col-span-1 xl:col-span-1">
+                    <TradingViewWidget
+                        scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js"
+                        config={TOP_STORIES_WIDGET_CONFIG}
+                        className="custom-chart"
+                        height={600}
+                    />
+                </div>
+                <div className="h-full md:col-span-1 xl:col-span-2">
+                    <TradingViewWidget
+                        scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js"
+                        config={MARKET_DATA_WIDGET_CONFIG}
+                        height={600}
+                    />
+                </div>
+            </section>
+        </div>
     );
 };
 
